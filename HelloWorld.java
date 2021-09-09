60daa52b6a7b4526faecdf14a025b2c782d5f5eb
MANOJ S T <56370924+manojsekharan@users.noreply.github.com>
2021-09-09 16:30:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,7 +35,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    System.out.println("this is madhu");
+    System.out.println("this is Manoj");
     System.out.println("Hello, Banglore");
     System.out.println("Hello, canada");
     System.out.println("Hello, kadapa ");
